988d34aed2bf2d196788ba3aa080b052c1705a42
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-06-03 15:11:32 -0700

    Add functionality for clear and backspace and overflow err

--- script.js
+++ script.js
@@ -38,7 +38,25 @@ function operate(operator, x, y) {
 // Display function
 function displayOnCalc(char) {
     displayArray.push(char);
+    if (char === 'CL') displayArray = [];
+    if (char === 'BACKSPACE') { displayArray.pop(); displayArray.pop(); }
+
+    //Throw error if overflow
+    if (displayArray.length > 12 || displayArray.includes('E')) {
+        displayArray = 'ERROR'.split('');
+    }
+    
+    //Display array as string in DOM or display '0' if string is empty
     display.textContent = displayArray.join('');
+    if (displayArray.length === 0) display.textContent = '0';
 }
 
 
+// Add Event Listners to buttons
+const calcButtons = document.querySelectorAll('button');
+console.log(calcButtons);
+calcButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        displayOnCalc(btn.textContent);
+    });
+});
