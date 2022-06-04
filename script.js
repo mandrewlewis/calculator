a40e37437396288ec724a1914182911eb3a6e367
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-06-03 23:58:26 -0700

    Fix bugs
    A lot of the bugs were found with mixing and matching special
    conditions to the operations. Negative numbers, decimals, using
    operation buttons without entering numbers, rounding, etc.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const display = document.querySelector('.display');
 let displayArray = [];
-
+let leftOp = [];
+let rightOp = [];
+let operator = '';
+let result = 0;
+let decimalFlag = false;
 
 // Operative functions
 function add(x, y) {
@@ -18,19 +22,23 @@ function divide(x, y) {
 function operate(operator, x, y) {
     switch(operator) {
         case '+':
-            add(x,y);
+            return add(x,y);
             break;
         case '-':
-            subtract(x,y);
+            return subtract(x,y);
             break;
-        case '*':
-            multiply(x,y);
+        case '×':
+            return multiply(x,y);
             break;
-        case '/':
-            divide(x,y);
+        case '÷':
+            if (y === 0) {
+                alert('Are you TRYING to break my calculator?\nYou must be so proud of yourself.');
+                return 'ERROR';
+            }
+            return divide(x,y);
             break;
         default:
-            alert('Critical error!');
+            return alert('Critical error!');
     }
 }
 
@@ -38,23 +46,83 @@ function operate(operator, x, y) {
 // Display function
 function displayOnCalc(char) {
     displayArray.push(char);
-    if (char === 'CL') displayArray = [];
-    if (char === 'BACKSPACE') { displayArray.pop(); displayArray.pop(); }
+
+    switch (char) {
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            displayArray.pop();
+            if (displayArray.length === 0){
+                //Creates negative number if '-' is pressed before any numbers
+                if(char === '-' && displayArray.length === 0) { 
+                    displayArray.push('-');
+                    break;
+                }
+                break;
+            }
+            else if (leftOp.length === 0) {
+                leftOp = +displayArray.join('');
+                displayArray = [];
+                decimalFlag = false;
+                operator = char;
+            } 
+            else {
+                //another + or - should be treated as = with result stored in leftOp
+                leftOp = operate(operator,leftOp,+displayArray.join(''));
+                displayArray = [];
+                decimalFlag = false;
+                operator = char;
+            }
+            break;
+        case '=':
+            displayArray.pop();
+            if (leftOp.length === 0) break;
+            if (displayArray.length === 0) break;
+
+            result = operate(operator,leftOp,+displayArray.join(''));
+            if (result.toString().includes('.')) { result = result.toFixed(1); }
+            displayArray = result.toString().split('');
+            leftOp = [];
+            decimalFlag = false;
+            break;
+        case 'CL':
+            displayArray = [];
+            leftOp = [];
+            decimalFlag = false;
+            break;
+        case 'BACKSPACE':
+            displayArray.pop();
+            displayArray.pop();
+            break;
+        case '.':
+            if (decimalFlag) displayArray.pop();
+            decimalFlag = true;
+            break;
+        default:
+    }
+    
 
     //Throw error if overflow
     if (displayArray.length > 12 || displayArray.includes('E')) {
         displayArray = 'ERROR'.split('');
     }
-    
-    //Display array as string in DOM or display '0' if string is empty
-    display.textContent = displayArray.join('');
-    if (displayArray.length === 0) display.textContent = '0';
+    //Display array as string in DOM or display '0' if string is empty or last num entered if operator
+    if (char === '+' || char === '-' || char === '×' || char === '÷' || char === '=') {
+        if (displayArray != ['-'] && displayArray.length > 0) {
+            display.textContent = leftOp;
+            display.textContent = displayArray.join('');
+        }
+    }
+    else if (displayArray.length === 0) {
+        display.textContent = '0';
+    }
+    else { display.textContent = displayArray.join(''); }
 }
 
 
 // Add Event Listners to buttons
 const calcButtons = document.querySelectorAll('button');
-console.log(calcButtons);
 calcButtons.forEach(btn => {
     btn.addEventListener('click', () => {
         displayOnCalc(btn.textContent);
